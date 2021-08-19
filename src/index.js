3558a18f369ff6bf3f61a80be1d937c3f513a2cb
Ivan <ivan_grekov@yahoo.com>
2021-08-19 09:38:26 +0300

    feat: add functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let numberAbs = Math.abs(n);
+    let arr1 = String(numberAbs).split('');
+    let arr2 = arr1.reverse();
+    let result = arr2.join('');
+    return result; 
 }
